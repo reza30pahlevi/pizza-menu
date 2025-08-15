7305ca1c02a34cd10b2635ab6502d65ca18eaf31
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-08-15 21:44:00 +0700

    latihan component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Spinaci</h2>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
