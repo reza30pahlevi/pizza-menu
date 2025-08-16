cef20102443594ff2b59bf85a9204e1320c3a07d
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-08-16 21:37:53 +0700

    props, component, jsx, styling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
   return (
-    <div>
-      <h2>Pizza Spinaci</h2>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+    <header className="header">
+      <h1>Reza Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={12}
+        photoName="pizzas/spinaci.jpg"
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
+  );
+}
+
+function Footer() {
+  // const hour = new Date().getHours();
+  // const openHour = 8;
+  // const closeHour = 22;
+  // const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (isOpen) alert("We're currently open");
+  // else alert("We're currently closed");
+
+  return (
+    <footer className="footer">
+      <p>{new Date().toLocaleTimeString()}. We are open!</p>
+    </footer>
   );
 }
 
